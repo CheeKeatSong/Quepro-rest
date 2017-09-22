6e46003baf1c7d00f804d8ade0796662d978085d
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-22 23:08:50 +0800

    Change

--- queries.js
+++ queries.js
@@ -5,6 +5,35 @@ var options = {
   promiseLib: promise
 };
 
+// var pgp = require('pg-promise')(options);
+// var connectionString = 'postgres://localhost:5432/puppies';
+// var db = pgp(connectionString);
+
+// add query functions
+
+// module.exports = {
+//   getAllPuppies: getAllPuppies,
+//   getSinglePuppy: getSinglePuppy,
+//   createPuppy: createPuppy,
+//   updatePuppy: updatePuppy,
+//   removePuppy: removePuppy
+// };
+
+// function getAllPuppies(req, res, next) {
+//   db.any('select * from pups')
+//     .then(function (data) {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           data: data,
+//           message: 'Retrieved ALL puppies'
+//         });
+//     })
+//     .catch(function (err) {
+//       return next(err);
+//     });
+// }
+
 var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://fricllllwdgdcz:c662f335a1a8860c37703282aceed22775e180a99935ad6a9b0a06247d0ad4db@ec2-204-236-239-225.compute-1.amazonaws.com:5432/dr861gshs4lib?ssl=true';
 var db = pgp(connectionString);
@@ -13,10 +42,10 @@ var db = pgp(connectionString);
 
 module.exports = {
   getAllRegistration: getAllRegistration,
-  // getSingleRegistration: getSingleRegistration,
-  // createRegistration: createRegistration,
-  // updateRegistration: updateRegistration,
-  // removeRegistration: removeRegistration
+  getSingleRegistration: getSingleRegistration,
+  createRegistration: createRegistration,
+  updateRegistration: updateRegistration,
+  removeRegistration: removeRegistration
 };
 
 function getAllRegistration(req, res, next) {
