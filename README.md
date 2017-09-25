40398f9572f5c3f0b92231520d029d4c60d4f810
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-25 19:34:16 +0800

    verification code

---
+++ README.md
@@ -0,0 +1,2 @@
+email verification - gunmail
+1. to send to unverified email address, credit card registration is needed
--- queries.js
+++ queries.js
@@ -71,33 +71,6 @@ function createRegistration(req, res, next) {
   var password = req.body.password;
   var mobileNumber = req.body.mobileNumber;
 
-  // verification code generator
-  var CodeGenerator = require('node-code-generator');
-  var generator = new CodeGenerator();
-  var pattern = '######';
-  var howMany = 1;
-  var options = {};
-  // Generate an array of random unique codes according to the provided pattern: 
-  var codes = generator.generateCodes(pattern, howMany, options);
-
-  // send email
-  var mailgun = require("mailgun-js");
-  var api_key = 'key-f05bf83bbab5abdaf494b79f996fd7c3';
-  var DOMAIN = 'sandbox0cff8999c890489eb0fe3704c00da3f5.mailgun.org';
-  var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
-
-  var data = {
-    from: 'QuePro <CKSong@quepro.com>',
-    to: 'cheekeatsong@gmail.com',
-    subject: 'Verify Your Account',
-    text: 'Your QuePro verification code is ' + codes
-  };
-
-  mailgun.messages().send(data, function (error, body) {
-    console.log(body);
-  });
-
-
 // Send SMS with twilio
   // const accountSid = 'AC9b778d92ad406516f2204e0698134b5d';
   //   const authToken = '2521fd35eab9c2fa1697976a4e9dce59';
@@ -131,48 +104,47 @@ function createRegistration(req, res, next) {
 // });
 
 
-
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-// nodemailer.createTestAccount((err, account) => {
-
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.ethereal.email',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: account.user, // generated ethereal user
-//             pass: account.pass  // generated ethereal password
-//         }
-//     });
-
-//     // setup email data with unicode symbols
-//     let mailOptions = {
-//         from: '"QuePro" <CKSong@quepro.com>', // sender address
-//         to: 'cheekeatsong@gmail.com', // list of receivers
-//         subject: 'Hello ✔', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: '<b>Hello world?</b>' // html body
-//     };
-
-//     // send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message sent: %s', info.messageId);
-//         // Preview only available when sending through an Ethereal account
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-//         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-//         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//     });
-// });
-
 db.none('INSERT INTO registration(userid, firstname, lastname, email, password, mobilenumber, verificationCode)' +
   'VALUES(DEFAULT, $1, $2, $3, $4, $5, $6)', [firstName, lastName, email, password, mobileNumber, parseInt(codes)])
 .then(function () {
+
+  // verification code generator
+  var CodeGenerator = require('node-code-generator');
+  var generator = new CodeGenerator();
+  var pattern = '######';
+  var howMany = 1;
+  var options = {};
+  // Generate an array of random unique codes according to the provided pattern: 
+  var codes = generator.generateCodes(pattern, howMany, options);
+
+  var nodemailer = require('nodemailer');
+  var transporter = nodemailer.createTransport();
+
+  transporter.sendMail({
+   from: 'CKSong@quepro.com',
+   to: 'cheekeatsong@gmail.com',
+   subject: 'Verify Your Account',
+   html: '<b>Verification Code</b>'
+   text: codes
+ }); 
+
+  // // send email
+  // var mailgun = require("mailgun-js");
+  // var api_key = 'key-f05bf83bbab5abdaf494b79f996fd7c3';
+  // var DOMAIN = 'sandbox0cff8999c890489eb0fe3704c00da3f5.mailgun.org';
+  // var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+
+  // var data = {
+  //   from: 'QuePro <CKSong@quepro.com>',
+  //   to: 'cheekeatsong@gmail.com',
+  //   subject: 'Verify Your Account',
+  //   text: 'Your QuePro verification code is ' + codes
+  // };
+
+  // mailgun.messages().send(data, function (error, body) {
+  //   console.log(body);
+  // });
+
   res.status(200)
   .json({
     status: 'success',
