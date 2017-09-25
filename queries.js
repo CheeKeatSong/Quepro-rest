ce81f11c17a42fd225599471223fe407a7ec88db
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-26 00:50:01 +0800

    verification code

--- queries.js
+++ queries.js
@@ -131,15 +131,13 @@ function resendEmailCode(req, res, next) {
   var DOMAIN = 'sandbox0cff8999c890489eb0fe3704c00da3f5.mailgun.org';
   var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
-  var dat = JSON.stringify(DBdata);
-  var num = parseInt(dat[0]);
-  var num1 = parseInt(dat[0][6]);
+  var arr = $.map(DBdata, function(el) { return el });
 
   var data = {
     from: 'QuePro <CKSong@queuepro.com>',
     to: '0116708@kdu-online.com',
     subject: 'Verify Your Account',
-    text: 'Your QuePro verification code is ' + num + num1
+    text: 'Your QuePro verification code is ' + arr[0].verificationCode
   };
 
   mailgun.messages().send(data, function (error, body) {
