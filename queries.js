2029d9a3200fca5b3cba7900ebb3f733814e7e06
CheeKeatSong <cheekeatsong@gmail.com>
2017-09-28 19:17:44 +0800

    1

--- queries.js
+++ queries.js
@@ -118,7 +118,7 @@ function accountVerification(req, res, next) {
 
     // if (arr[0].verificationcode == accountVerificationCode){
 
-      db.one('INSERT INTO User(userid, firstname, lastname, email, password, mobilenumber, verificationCode, smsInterval, smsActivation, pushInterval, pushActivation, points)' +
+      db.none('INSERT INTO User(userid, firstname, lastname, email, password, mobilenumber, verificationCode, smsInterval, smsActivation, pushInterval, pushActivation, points)' +
         'VALUES(DEFAULT, $1, $2, $3, $4, $5, 60, true, 60, true, ) RETURNING userId', [arr[0].firstname, arr[0].lastname, arr[0].email, arr[0].password, arr[0].mobilenumber]);
     //   .then(function () {
 
